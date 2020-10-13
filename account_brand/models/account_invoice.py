e2047b76b63763227c1719251abfbd01ec6bbda4
jguenat <julien.guenat@geckoescalade.ch>
2020-10-13 23:59:43 +0200

    resolve conflicts

--- account_brand/models/account_invoice.py
+++ account_brand/models/account_invoice.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2019 Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models, api
+from odoo import api, fields, models
 
 
 class AccountInvoice(models.Model):
@@ -24,3 +24,21 @@ class AccountInvoice(models.Model):
     @api.onchange('brand_name')
     def _onchange_brand_name(self):
         self.brand_id = self.brand_name.brand
+
+
+    @api.multi
+    def _is_brand_required(self):
+        self.ensure_one()
+        if self.type in ('in_invoice', 'in_refund'):
+            return False
+        return super(AccountInvoice, self)._is_brand_required()
+
+    @api.model
+    def _prepare_refund(self, invoice, date_invoice=None, date=None,
+                        description=None, journal_id=None):
+        values = super(AccountInvoice, self)._prepare_refund(
+            invoice, date_invoice=date_invoice, date=date,
+            description=description, journal_id=journal_id)
+        if invoice.brand_id:
+            values['brand_id'] = invoice.brand_id.id
+        return values
